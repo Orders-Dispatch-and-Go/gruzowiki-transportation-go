5319c3e92499a520ed8de4233b3f76730b1d1ea3
Joerg <y.vegner@g.nsu.ru>
2025-11-17 20:39:47 +0700

    наебал, теперь точно ready

--- rest/models/models.go
+++ rest/models/models.go
@@ -1,95 +1,95 @@
 package models
 
 type CreateCarRequest struct {
-	Type      string
-	Length    int32
-	Width     int32
-	Height    int32
-	MaxWeight int32
-	Number    string
-	OwnerID   int32
+	Type      string `json:"type"`
+	Length    int32  `json:"length"`
+	Width     int32  `json:"width"`
+	Height    int32  `json:"height"`
+	MaxWeight int32  `json:"maxweight"`
+	Number    string `json:"number"`
+	OwnerID   int32  `json:"ownerid"`
 }
 
 type CreateCarResponse struct {
-	ID int32
+	ID int32 `json:"id"`
 }
 
 type GetCarResponse struct {
-	ID        int32
-	Type      string
-	Length    int32
-	Width     int32
-	Height    int32
-	MaxWeight int32
-	Number    string
-	OwnerID   int32
+	ID        int32  `json:"id"`
+	Type      string `json:"type"`
+	Length    int32  `json:"length"`
+	Width     int32  `json:"width"`
+	Height    int32  `json:"height"`
+	MaxWeight int32  `json:"maxweight"`
+	Number    string `json:"number"`
+	OwnerID   int32  `json:"ownerid"`
 }
 
 type UpdateCarRequest struct {
-	Type      *string
-	Length    *int32
-	Width     *int32
-	Height    *int32
-	MaxWeight *int32
-	Number    *string
-	OwnerID   *int32
+	Type      *string `json:"type"`
+	Length    *int32  `json:"length"`
+	Width     *int32  `json:"width"`
+	Height    *int32  `json:"height"`
+	MaxWeight *int32  `json:"maxweight"`
+	Number    *string `json:"number"`
+	OwnerID   *int32  `json:"ownerid"`
 }
 
 type UpdateCarResponse struct {
-	ID int32
+	ID int32 `json:"id"`
 }
 
 type CreateCarrierRequest struct {
-	ID             int32
-	DriverCategory string
+	ID             int32  `json:"id"`
+	DriverCategory string `json:"drivercategory"`
 }
 
 type CreateCarrierResponse struct {
-	ID int32
+	ID int32 `json:"id"`
 }
 
 type GetCarrierResponse struct {
-	ID             int32
-	DriverCategory string
+	ID             int32  `json:"id"`
+	DriverCategory string `json:"drivercategory"`
 }
 
 type UpdateCarrierRequest struct {
-	DriverCategory string
+	DriverCategory string `json:"drivercategory"`
 }
 
 type UpdateCarrierResponse struct {
-	ID int32
+	ID int32 `json:"id"`
 }
 
 type CreateRecipientRequest struct {
-	FirstName  string
-	SecondName string
-	ThirdName  string
-	Phone      string
-	Email      string
+	FirstName  string `json:"firstname"`
+	SecondName string `json:"secondname"`
+	ThirdName  string `json:"thirdname"`
+	Phone      string `json:"phone"`
+	Email      string `json:"email"`
 }
 
 type CreateRecipientResponse struct {
-	ID int32
+	ID int32 `json:"id"`
 }
 
 type GetRecipientResponse struct {
-	ID         int32
-	FirstName  string
-	SecondName string
-	ThirdName  string
-	Phone      string
-	Email      string
+	ID         int32  `json:"id"`
+	FirstName  string `json:"firstname"`
+	SecondName string `json:"secondname"`
+	ThirdName  string `json:"thirdname"`
+	Phone      string `json:"phone"`
+	Email      string `json:"email"`
 }
 
 type UpdateRecipientRequest struct {
-	FirstName  string
-	SecondName string
-	ThirdName  string
-	Phone      string
-	Email      string
+	FirstName  string `json:"firstname"`
+	SecondName string `json:"secondname"`
+	ThirdName  string `json:"thirdname"`
+	Phone      string `json:"phone"`
+	Email      string `json:"email"`
 }
 
 type UpdateRecipientResponse struct {
-	ID int32
+	ID int32 `json:"id"`
 }
