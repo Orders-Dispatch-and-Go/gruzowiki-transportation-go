b9b3b239d9219365a71e3185b8b61f2d86eecf22
e.davydov <davidoveg@gmail.com>
2025-11-23 14:19:48 +0700

    hz

--- util/numericUtil.go
+++ util/numericUtil.go
@@ -10,3 +10,16 @@ func ToNumeric(decimalNum decimal.Decimal) pgtype.Numeric {
 	_ = price.Scan(decimalNum.String())
 	return price
 }
+
+func NumericToString(n pgtype.Numeric) string {
+	numLen := len(n.Int.String())
+	return n.Int.String()[:numLen-2] + "." + n.Int.String()[numLen-2:]
+}
+
+func PgInt8ToInt(n pgtype.Int8) int64 {
+	return n.Int64
+}
+
+func PgInt4ToInt(n pgtype.Int4) int {
+	return int(n.Int32)
+}
