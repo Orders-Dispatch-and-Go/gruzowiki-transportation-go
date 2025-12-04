4dd00ff25705488de0e8a1647353f06d7ec3db23
YARO <lolprikol09@mail.ru>
2025-12-05 03:36:35 +0700

    add body log

--- rest/middlewares/midlwares.go
+++ rest/middlewares/midlwares.go
@@ -1,9 +1,12 @@
 package middlewares
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"gruzowiki/rest/terror"
+	"io"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -38,11 +41,33 @@ func HandleError(next echo.HandlerFunc) echo.HandlerFunc {
 func LoggingMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		fmt.Println("==========================================")
-		fmt.Println("Request:")
-		fmt.Println("Method:", c.Request().Method)
-		fmt.Println("URI:", c.Request().URL.Path)
-		fmt.Println("Query:", c.Request().URL.RawQuery)
-
+        fmt.Println("Request:")
+        fmt.Println("Method:", c.Request().Method)
+        fmt.Println("URI:", c.Request().URL.Path)
+        fmt.Println("Query:", c.Request().URL.RawQuery)
+        
+        if c.Request().Body != nil {
+            bodyBytes, err := io.ReadAll(c.Request().Body)
+            if err != nil {
+                fmt.Println("Error reading request body:", err)
+            } else {
+                c.Request().Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
+                
+                if len(bodyBytes) > 0 {
+                    fmt.Println("Request Body:")
+                    if json.Valid(bodyBytes) {
+                        var prettyJSON bytes.Buffer
+                        if err := json.Indent(&prettyJSON, bodyBytes, "", "  "); err == nil {
+                            fmt.Println(prettyJSON.String())
+                        } else {
+                            fmt.Println(string(bodyBytes))
+                        }
+                    } else {
+                        fmt.Println(string(bodyBytes))
+                    }
+                }
+            }
+        }
   		err := next(c)
   		if err != nil {
 			fmt.Println("Error:", err.Error())
