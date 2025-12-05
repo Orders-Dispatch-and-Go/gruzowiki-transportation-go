c5d74b3456149352b9a9c5654c43394a79ff83ad
e.davydov <davidoveg@gmail.com>
2025-12-05 19:40:44 +0700

    add error logs

--- rest/middlewares/midlwares.go
+++ rest/middlewares/midlwares.go
@@ -34,6 +34,10 @@ func HandleError(next echo.HandlerFunc) echo.HandlerFunc {
 			code = http.StatusInternalServerError
 		}
 
+		fmt.Println("Request ERROR")
+		fmt.Printf("Method: %s URI: %s\n", c.Request().Method, c.Request().URL.Path)
+		fmt.Printf("error: %s, code: %d\n", err, code)
+
 		return c.JSON(code, err)
 	}
 }
@@ -41,39 +45,39 @@ func HandleError(next echo.HandlerFunc) echo.HandlerFunc {
 func LoggingMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		fmt.Println("==========================================")
-        fmt.Println("Request:")
-        fmt.Println("Method:", c.Request().Method)
-        fmt.Println("URI:", c.Request().URL.Path)
-        fmt.Println("Query:", c.Request().URL.RawQuery)
-        
-        if c.Request().Body != nil {
-            bodyBytes, err := io.ReadAll(c.Request().Body)
-            if err != nil {
-                fmt.Println("Error reading request body:", err)
-            } else {
-                c.Request().Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
-                
-                if len(bodyBytes) > 0 {
-                    fmt.Println("Body:")
-                    if json.Valid(bodyBytes) {
-                        var prettyJSON bytes.Buffer
-                        if err := json.Indent(&prettyJSON, bodyBytes, "", "  "); err == nil {
-                            fmt.Println(prettyJSON.String())
-                        } else {
-                            fmt.Println(string(bodyBytes))
-                        }
-                    } else {
-                        fmt.Println(string(bodyBytes))
-                    }
-                }
-            }
-        }
-  		err := next(c)
-  		if err != nil {
+		fmt.Println("Request:")
+		fmt.Println("Method:", c.Request().Method)
+		fmt.Println("URI:", c.Request().URL.Path)
+		fmt.Println("Query:", c.Request().URL.RawQuery)
+
+		if c.Request().Body != nil {
+			bodyBytes, err := io.ReadAll(c.Request().Body)
+			if err != nil {
+				fmt.Println("Error reading request body:", err)
+			} else {
+				c.Request().Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
+
+				if len(bodyBytes) > 0 {
+					fmt.Println("Body:")
+					if json.Valid(bodyBytes) {
+						var prettyJSON bytes.Buffer
+						if err := json.Indent(&prettyJSON, bodyBytes, "", "  "); err == nil {
+							fmt.Println(prettyJSON.String())
+						} else {
+							fmt.Println(string(bodyBytes))
+						}
+					} else {
+						fmt.Println(string(bodyBytes))
+					}
+				}
+			}
+		}
+		err := next(c)
+		if err != nil {
 			fmt.Println("Error:", err.Error())
 			return err
-  		}
-	
-  		return nil
- 	}
+		}
+
+		return nil
+	}
 }
